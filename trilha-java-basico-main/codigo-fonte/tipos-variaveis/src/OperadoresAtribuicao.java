8be57c0a31dea8064a1986a82555b06f397f7dd6
Antonio Fava <afavajrbr80@gmail.com>
2022-10-06 17:14:54 -0400

    operadores atribuição

---
+++ trilha-java-basico-main/codigo-fonte/tipos-variaveis/src/OperadoresAtribuicao.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+    }
+}
