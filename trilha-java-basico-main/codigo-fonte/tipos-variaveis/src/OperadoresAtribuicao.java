bed8ff14093e63ef2b8996719b7beedf2566d418
Antonio Fava <afavajrbr80@gmail.com>
2022-10-06 17:21:11 -0400

    Operadores

--- trilha-java-basico-main/codigo-fonte/tipos-variaveis/src/OperadoresAtribuicao.java
+++ trilha-java-basico-main/codigo-fonte/tipos-variaveis/src/OperadoresAtribuicao.java
@@ -1,10 +1,11 @@
-public class Operadores {
+import java.sql.Date;
+
+public class OperadoresAtribuicao {
     public static void main(String[] args) {
         String nome = "GLEYSON";
         int idade = 22;
         double peso = 68.5;
         char sexo = 'M';
         boolean doadorOrgao = false;
-        Date dataNascimento = new Date();
-    }
+      }
 }
